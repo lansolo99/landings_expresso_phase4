523add8dd06be9ae9fe89ff053c1b30be2fe9fcb
Stéphane Changarnier <lansolo99@hotmail.fr>
2016-06-17 09:03:27 +0300

    dffd

---
+++ js/vente_flash_popin.js
@@ -0,0 +1,62 @@
+(function($) {
+  'use strict';
+  $("a[href='#simulation_popin']").fancybox({
+    padding:10,
+    margin:20,
+    opacity:false,
+    showCloseButton:true,
+    width:840,
+    height:900,
+    autoScale:true,
+    autoDimensions:false,
+    centerOnScroll:false,
+    hideOnOverlayClick:true,
+    hideOnContentClick:false,
+    overlayOpacity:0.3,
+    overlayColor:"#666",
+    transitionIn:"",
+    transitionOut:"",
+    speedIn:500,
+    speedOut:500,
+    showCloseButton:true,
+    showNavArrows:false,
+    enableEscapeButton:true,
+    titleShow: false,
+    onStart:function() {
+    },
+    onComplete:function() {       
+    },
+    onClosed:function() {
+    } 
+  }); 
+  $("a[href='#simulation_bfm_popin']").fancybox({
+    padding:10,
+    margin:20,
+    opacity:false,
+    showCloseButton:true,
+    width:840,
+    height:900,
+    autoScale:true,
+    autoDimensions:false,
+    centerOnScroll:false,
+    hideOnOverlayClick:true,
+    hideOnContentClick:false,
+    overlayOpacity:0.3,
+    overlayColor:"#666",
+    transitionIn:"",
+    transitionOut:"",
+    speedIn:500,
+    speedOut:500,
+    showCloseButton:true,
+    showNavArrows:false,
+    enableEscapeButton:true,
+    titleShow: false,
+    onStart:function() {
+    },
+    onComplete:function() {        
+    },
+    onClosed:function() {
+    } 
+  }); 
+}(jQuery));
+  
